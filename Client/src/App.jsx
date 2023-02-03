59b1d456d805e9dbb975b5789c452407c1dc6fa1
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-02-03 14:48:06 +0500

    Create Front-End

---
+++ Client/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import ChatBody from './Components/ChatBody.jsx';
+import ChatInput from './Components/ChatInput.jsx';
+
+const App = () => {
+
+    const [chat, setChat] = useState([]);
+
+    return (
+        <div className="bg-[#101822] h-screen py-6 relative sm:px-16 px-12 
+        text-white overflow-hidden flex flex-col justify-between 
+        align-middle">
+
+            <div className="gradient-01 z-0 absolute"></div>
+            <div className="gradient-02 z-0 absolute"></div>
+
+            <div className="uppercase font-bold  text-2xl text-center mb-3">
+                Chat GPT OpenAi
+            </div>
+
+            <div className="h-[90%] overflow-auto w-full max-w-4xl min-w-[20rem] py-8 px-4 self-center
+            scrollbar-thumb-slate-400 scrollbar-thin scrollbar-track-gray-tranparent scrollbar-thumb-rounded-md ">
+                <ChatBody />
+            </div>
+
+            <div className="w-full max-w-4xl min-w-[20rem] self-center">
+                <ChatInput />
+            </div>
+        </div>
+    )
+};
+
+export default App;
---
+++ Client/src/Components/ChatInput.jsx
@@ -0,0 +1,21 @@
+const ChatInput = () => {
+    return (
+        <div className="w-full bg-white bg-opacity-10 max-h-40 rounded-lg
+        px-4 py-4 overflow-auto relative">
+
+            <textarea
+                rows={1}
+                className="border-0 bg-transparent outline-none resize-none w-11/12"
+            />
+
+            <img
+                src="./send.png"
+                alt="send-btn"
+                width={20}
+                className="absolute top-5 right-4 hover:cursor-pointer ease-in duration-100 hover:scale-125"
+            />
+        </div>
+    )
+};
+
+export default ChatInput;
