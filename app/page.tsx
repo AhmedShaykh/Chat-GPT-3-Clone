d7daa1e3b2fbd1ec4af34af505319bc98a3d9db4
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-02-17 01:26:51 +0500

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+const Home: FC = () => {
+    return (
+        <main>
+            <h1>
+                Chat GPT 3
+            </h1>
+        </main>
+    )
+};
+
+export default Home;
