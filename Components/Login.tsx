373fda1c6f1d8e676bfca61a0c20297c67bd5696
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-03-05 23:26:31 +0500

    Final Commit

--- Components/Login.tsx
+++ Components/Login.tsx
@@ -6,12 +6,14 @@ import Image from 'next/image';
 const Login = () => {
     return (
         <div className="screenBg h-screen flex flex-col items-center justify-center text-center">
-            <Image
-                src="https://links.papareact.com/2i6"
-                width={300}
-                height={300}
-                alt="logo"
-            />
+            <div>
+                <Image
+                    src="https://links.papareact.com/2i6"
+                    width={300}
+                    height={300}
+                    alt="logo"
+                />
+            </div>
             <button
                 className="text-white font-bold text-3xl animate-pulse"
                 onClick={() => signIn("google")}
