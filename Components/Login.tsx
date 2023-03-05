6d63b96e4ff2cc0be1dc49a46e6fc103708b27c9
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-03-05 22:54:16 +0500

    Final Commit

--- Components/Login.tsx
+++ Components/Login.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 const Login = () => {
     return (
-        <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
+        <div className="screenBg h-screen flex flex-col items-center justify-center text-center">
             <Image
                 src="https://links.papareact.com/2i6"
                 width={300}
